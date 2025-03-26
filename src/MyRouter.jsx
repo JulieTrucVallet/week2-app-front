38a6ffbb802bbae4a25ec75846f82788fa9930ee
Julie <julie.trucvallet@gmail.com>
2025-03-26 14:54:26 +0100

    updated

--- src/MyRouter.jsx
+++ src/MyRouter.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router'
 import App from './App'
 import NavBar from './components/NavBar'
+import Login from './pages/Login'
 import Profile from './pages/Profile'
 import Register from './pages/Register'
 
@@ -12,6 +13,7 @@ const MyRouter = () => {
                 <Route path='/' element={<App />} />
                 <Route path='/profile' element={<Profile />} />
                 <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
             </Routes>
         </>
     )
