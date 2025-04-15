04a4afbeb1ae774bb86338d017275394b67a0864
Julie <julie.trucvallet@gmail.com>
2025-04-15 15:52:34 +0200

    Updated

--- src/MyRouter.jsx
+++ src/MyRouter.jsx
@@ -1,21 +1,33 @@
 import { Route, Routes } from 'react-router'
 import App from './App'
 import NavBar from './components/NavBar'
-import AddEvent from './pages/AddService'
+import { default as AddEvent, default as AddService } from './pages/AddService'
 import Login from './pages/Login'
 import Profile from './pages/Profile'
 import Register from './pages/Register'
+import ProtectedRoute from './utils/ProtectedRoute'
 
 const MyRouter = () => {
     return(
         <>
             <NavBar />
             <Routes>
-                <Route path='/' element={<App />} />
-                <Route path='/profile' element={<Profile />} />
+                <Routes exact path="/" element={<App />} />
+                <Route path='/profile' element={
+                    <ProtectedRoute>
+                        <Profile />
+                    </ProtectedRoute>
+                } />
                 <Route path='/register' element={<Register />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/addevent' element={<AddEvent />} />
+                <Route path='/addservice' element={
+                    <ProtectedRoute>
+                        <AddService />
+                    </ProtectedRoute>
+                } />
+                <Route path='/service/:id' element={<ServiceDetails />} />
+                <Route path='*' element={<p>404 not found</p>} />
             </Routes>
         </>
     )
